6b3aa3d8fb72ab1d26e1bfb57415006c450bae44
OmarAraby <omararaby789@gmail.com>
2023-11-07 21:00:35 +0200

    Contact App Complete

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,39 @@
 from django.shortcuts import render
-
+from .models import Info
+from django.core.mail import send_mail
+from django.conf import settings
 # Create your views here.
 
 def send_message(request):
-    pass
+
+    myinfo = Info.objects.last()
+
+    if request.method=='POST':
+        subject = request.POST['subject']
+        email = request.POST['email']
+        message = request.POST['message']
+
+
+   
+
+        email_subject = 'New Contact Submission '
+        message = 'Subject : {}\nEmail :  {}\n\nMessage :\n{}'.format(subject, email, message)
+        from_email = settings.DEFAULT_FROM_EMAIL
+        recipient_list = [settings.CONTACT_EMAIL]
+
+           
+           
+        send_mail(
+            subject=email_subject,
+            message=message,
+            from_email=from_email,
+            recipient_list=recipient_list,
+            fail_silently=False,
+            
+        )
+
+    
+
+
+
+    return render(request,'contact/contact.html',{'myinfo':myinfo})  
--- job/views.py
+++ job/views.py
@@ -6,6 +6,7 @@ from .models import Job
 from django.core.paginator import Paginator
 from .form import ApplyForm ,JobForm
 from django.contrib.auth.decorators import login_required
+from .filters import JobFilter
 
 # Create your views here.
 
@@ -13,12 +14,20 @@ from django.contrib.auth.decorators import login_required
 def job_list(request):
     job_list = Job.objects.all()
 
+   ### filters
+    myfilter = JobFilter(request.GET, queryset=job_list)
+    job_list = myfilter.qs
+
+
 
     paginator = Paginator(job_list, 3)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
-    context = {'jobs': page_obj , 'job_list':job_list}
+
+ 
+
+    context = {'jobs': page_obj , 'myfilter':myfilter,'job_list':job_list}
     return render(request , 'job/job_list.html', context ) 
     
 
